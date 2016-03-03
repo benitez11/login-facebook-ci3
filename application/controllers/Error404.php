99d18c17d432c6165c293395240315fdf892b2c7
benitez11 <benitez11@gmail.com>
2016-03-03 13:22:55 -0600

    Se corrige error404

--- application/controllers/Error404.php
+++ application/controllers/Error404.php
@@ -6,9 +6,7 @@ class Error404 extends CI_Controller {
 	
    public function index(){   	
    	$this->template->set_layout("temas/error404.php"); 
-   	$this->template->loadContent("admin/sv_dashboard", array());  	
-   	
-      //echo 'Error 404_____>. Usted está intentando acceder a una página que no existe.';
+   	$this->template->loadContent("admin/sv_dashboard", array());  	   	
    }
 
 }
