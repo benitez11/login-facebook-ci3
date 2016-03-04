cd71efe1cebc7a3b9d95121c27ee39d262c92119
benitez11 <benitez11@gmail.com>
2016-03-04 13:20:21 -0600

    cambio2

--- application/controllers/Error404.php
+++ application/controllers/Error404.php
@@ -4,15 +4,12 @@ if (!defined('BASEPATH'))
 
 class Error404 extends CI_Controller { 
 	
-   public function index(){   	
+   public function index(){   
+   
    	$this->template->set_layout("temas/error404.php"); 
    	$this->template->loadContent("admin/sv_dashboard", array());  	
    	
-      //echo 'Error 404_____>. Usted está intentando acceder a una página que no existe.';
-	  //echo 'Error 404_____>. Usted está intentando acceder a una página que no existe.';
-	  //echo 'Error 404_____>. Usted está intentando acceder a una página que no existe.';	  
-   
-   
+      //echo 'Error 404_____>. Usted está intentando acceder a una página que no existe.';   
    }
 
 }
